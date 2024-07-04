4eaac4230341c118a5f87c417ef201cf91edddb2
Ai Waifu <aiwaifu@protonmail.com>
2024-07-04 17:43:16 +0000

    wip diffusion ar

--- catfish/lvd/diffusion_ar.py
+++ catfish/lvd/diffusion_ar.py
@@ -1,10 +1,11 @@
-""""""
 import os
+import time
 import re
 import collections
 
 import fs
 import jax
+import jax.tree_util as jtu
 import jax.numpy as jnp
 import optax
 import pickle as pkl
@@ -15,7 +16,7 @@ import catfish.lvd.shrd_data_loader as sdl
 import catfish.lvd.diffusion_core as dc
 
 
-DAEModel = collections.namedtuple(["encoder", "decoder"])
+DAEModel = collections.namedtuple('DAEModel', ['encoder', 'decoder'])
 
 class DiffARHarness:
     """Sharded Diffusion autoencoder harness"""
@@ -29,14 +30,29 @@ class DiffARHarness:
         self.dist_manager = None
         self.data_loader = None
         self.credentials_path = None
-        self.data_fs = None
+        self.worker_fs_args = None
         self.ckpt_fs = None
 
+        print("Parsing arguments...")
         self.parse_args()
+        
+        print("Initializing file system...")
         self.init_fs()
+
+        print("Initializing sharding manager...")
         self.init_dist_manager()
+        
+        print("Initializing data_loader...")
         self.init_data_loader()
+        
+        print("Creating model...")
         self.make_model()
+
+        print("Creating Optimizer...")
+        self.make_optimizer()
+
+    def parse_args(self):
+        pass
 
     def init_fs(self):
         gcp_conf = self.cfg["gcp"]
@@ -49,19 +65,24 @@ class DiffARHarness:
         dl_root_directory = dl_conf["data_root_directory"]
 
         if dl_fs_type == "local":
-            self.data_fs = sdl.os_filesystem(dl_root_directory)
+            self.worker_fs_args = {
+                "fs_type": "os",
+                "root_path": dl_root_directory
+            }
         elif dl_fs_type == "gcp":
-            self.data_fs = sdl.gcp_filesystem(
-                gcp_bucket_name, 
-                root_path=dl_root_directory, 
-                gcp_credentials_path=gcp_credentials_path)
+            self.worker_fs_args = {
+                "fs_type": "gcp",
+                "bucket_name": gcp_bucket_name,
+                "root_path": dl_root_directory,
+                "credentials_path": gcp_credentials_path
+            }
         else:
             raise Exception(f"Invalid fs_type provided, provided {dl_fs_type}")
         
         #Initialize checkpoint filesystem
         ckpt_conf = self.cfg["diffusion_auto_encoder"]["checkpoints"]
         ckpt_fs_type = ckpt_conf["fs_type"]
-        ckpt_root_directory = ckpt_conf["data_root_directory"]
+        ckpt_root_directory = ckpt_conf["ckpt_root_directory"]
         
         if ckpt_fs_type == "local":
             self.ckpt_fs = sdl.os_filesystem(ckpt_root_directory)
@@ -73,31 +94,51 @@ class DiffARHarness:
         else:
             raise Exception(f"Invalid fs_type provided, provided {ckpt_root_directory}")
 
-    def init_data_loader(self, mode):
-        if mode == "train":
-            def worker_interface_factory():
-                lwi = sdl.LatentWorkerInterface(self.data_fs)
-                return lwi
+    def init_data_loader(self):
+        operation = self.args.operation
+        dl_conf = self.cfg["diffusion_auto_encoder"]["data_loader"]
+
+        if operation == "train_dae":
+            worker_interface_cls = sdl.ImageWorkerInterface
             
             def shard_interface_factory():
-                lwi = sdl.ImageShardInterface(self.dist_manager)
-                return lwi
+                isi = sdl.ImageShardInterface(self.dist_manager)
+                return isi
+        
+        elif operation == "autoencode":
+            worker_interface_cls = sdl.VideoWorkerInterface
+            
+            def shard_interface_factory():
+                isi = sdl.VideoShardInterface(self.dist_manager)
+                return isi
+        else:
+            raise ValueError(f"Unsupported operation {operation}")
+
         
         self.sharded_data_downloader =  sdl.ShardedDataDownloader(
-            worker_interface_factory,
+            self.worker_fs_args,
+            worker_interface_cls,
             shard_interface_factory,
             self.dist_manager,
+            workers_per_node=dl_conf["workers_per_node"],
+            batch_size=dl_conf["batch_size"],
+            queue_depth=dl_conf["queue_depth"],
         )
 
     def init_dist_manager(self):
-        dm_cfg = self.cfg["dist_manager"]
+        dm_cfg = self.cfg["diffusion_auto_encoder"]["dist_manager"]
 
         mesh_shape = dm_cfg["mesh_shape"]
 
-        self.dist_manager = du.DistManager(mesh_shape, self.credientials_path)
+        self.dist_manager = du.DistManager(mesh_shape, self.ckpt_fs)
     
     def make_model(self):
-        model_conf = self.conf["transformer_ardm"]["model"]
+        model_conf = self.cfg["diffusion_auto_encoder"]["model"]
+        enc_conf = model_conf["encoder"]
+        dec_conf = model_conf["decoder"]
+
+        seed = self.cfg["seed"]
+        self.state["prng_key"] = self.dist_manager.get_key(seed)
         
         self.state["prng_key"], enc_key, dec_key = jax.random.split(self.state["prng_key"],3)
 
@@ -119,120 +160,68 @@ class DiffARHarness:
     def make_optimizer(self):
         opt_cfg = self.cfg["diffusion_auto_encoder"]["train"]
         
-        self.optimizer = optax.adam(lr=opt_cfg["lr"])
-        self.state["opt_state"] = self.optimizer.init(self.model)
+        self.optimizer = optax.adam(learning_rate=opt_cfg["lr"])
+        self.state["opt_state"] = self.optimizer.init(self.state["model"])
     
-    def _list_checkpoints(self):
+    def list_checkpoints(self):
         """List all checkpoint directories."""
         try:
             directories = [d for d in self.ckpt_fs.listdir('/') if self.ckpt_fs.isdir(d)]
-            checkpoint_dirs = [d for d in directories if d.startswith('ckpt_')]
+            checkpoint_dirs = [f"/{d}" for d in directories if d.startswith('ckpt_')]
             return sorted(checkpoint_dirs, key=lambda x: int(x.split('_')[1]))
         except fs.errors.ResourceNotFound:
             return []
 
-    def save_checkpoint(self, step):
+    def save_checkpoint(self, path):
         """Save a checkpoint at the given step."""
-        path = self.new_ckpt_path(step)
         
-        # Ensure the checkpoint directory exists
-        self.ckpt_fs.makedirs(path, recreate=True)
+        ckpt_file_path = f"{path}/ckpt.pkl"
+        sharding_pytree = self.dist_manager.get_pytree_sharding(self.state)
+        self.dist_manager.save_pytree(self.state, sharding_pytree, ckpt_file_path)
 
-        # Save model
-        model_path = f"{path}/model"
-        self.ckpt_fs.makedirs(f"{model_path}/encoder", recreate=True)
-        self.ckpt_fs.makedirs(f"{model_path}/decoder", recreate=True)
-        self.state["model"].encoder.save(f"{model_path}/encoder")
-        self.state["model"].decoder.save(f"{model_path}/decoder")
-
-        # Save optimizer state
-        opt_path = f"{path}/opt_state"
-        self.ckpt_fs.makedirs(opt_path, recreate=True)
-
-        def save_opt_state(opt_state, prefix):
-            for key, value in opt_state.items():
-                if isinstance(value, (jnp.ndarray, jax.Array)):
-                    self.dist_manager.save_array(value, self.dist_manager.uniform_sharding, f"{prefix}/{key}")
-                elif isinstance(value, (dict, optax.EmptyState)):
-                    save_opt_state(value, f"{prefix}/{key}")
-                else:
-                    # For scalar values or other types, save using regular pickle
-                    with self.ckpt_fs.open(f"{prefix}/{key}.pkl", 'wb') as f:
-                        pkl.dump(value, f)
-
-        save_opt_state(self.state["opt_state"], opt_path)
-
-        # Save PRNG key
-        self.dist_manager.save_array(self.state["prng_key"], self.dist_manager.uniform_sharding, f"{path}/prng_key")
-
-    def load_checkpoint(self, path=None):
-        """Load a checkpoint from the given path or the latest if not specified."""
-        if path is None:
-            path = self.latest_ckpt_path()
-        
-        if path is None:
-            raise ValueError("No checkpoint found to load.")
-
-        # Load model
-        model_path = f"{path}/model"
-        self.state["model"].encoder.load(f"{model_path}/encoder")
-        self.state["model"].decoder.load(f"{model_path}/decoder")
-
-        # Load optimizer state
-        opt_path = f"{path}/opt_state"
-
-        def load_opt_state(prefix):
-            opt_state = {}
-            for item in self.ckpt_fs.listdir(prefix):
-                item_path = f"{prefix}/{item}"
-                if self.ckpt_fs.isdir(item_path):
-                    opt_state[item] = load_opt_state(item_path)
-                elif item.endswith('.pkl'):
-                    with self.ckpt_fs.open(item_path, 'rb') as f:
-                        opt_state[item[:-4]] = pkl.load(f)
-                else:
-                    opt_state[item] = self.dist_manager.load_array(self.dist_manager.uniform_sharding, item_path)
-            return opt_state
-
-        self.state["opt_state"] = load_opt_state(opt_path)
-
-        # Reconstruct the OptState object
-        self.state["opt_state"] = jtu.tree_map(
-            lambda x: x if isinstance(x, (optax.EmptyState, dict)) else x,
-            self.state["opt_state"]
-        )
-
-        # Load PRNG key
-        self.state["prng_key"] = self.dist_manager.load_array(self.dist_manager.uniform_sharding, f"{path}/prng_key")
-
+    def load_checkpoint(self, path):
+        """Load a checkpoint from the given path"""
+        ckpt_file_path = f"{path}/ckpt.pkl"
+        sharding_pytree = self.dist_manager.get_pytree_sharding(self.state)
+        self.state = self.dist_manager.load_pytree(sharding_pytree, ckpt_file_path)
     
-    def most_recent_ckpt(self):
+    def latest_ckpt_step(self):
         """Get the most recent checkpoint number."""
-        checkpoints = self._list_checkpoints()
+        checkpoints = self.list_checkpoints()
         if not checkpoints:
-            return 0
+            return None 
         return int(checkpoints[-1].split('_')[1])
-
-
-    def new_ckpt_path(self, id):
-        ckpt_n = self.most_recent_ckpt()
-
-        ckpt_freq = self.cfg["diffusion_autoencoder"]["train"]["ckpt_freq"]
-        
-        new_ckpt_n = ckpt_n + ckpt_freq
-        assert id == new_ckpt_n
-
-        os.path.join(self.args.ckpt_path,"ckpt_path")
 
     def latest_ckpt_path(self):
         """Get the path of the latest checkpoint."""
-        checkpoints = self._list_checkpoints()
+        checkpoints = self.list_checkpoints()
         if not checkpoints:
             return None
-        return f'/{checkpoints[-1]}'
+        return checkpoints[-1]
+    
+    def ckpt_path(self, step):
+        return f"/ckpt_{step}"
+
+    def new_ckpt_path(self):
+
+        ckpt_n = self.most_recent_ckpt_step()
+
+        ckpt_freq = self.cfg["diffusion_auto_encoder"]["train"]["ckpt_freq"]
+        
+        new_ckpt_n = ckpt_n + ckpt_freq
+
+        new_ckpt_path = f"/ckpt_{new_ckpt_n}"
+        return new_ckpt_path
     
 
     def train(self):
+        def loss_fn(model, data, subkey):
+            latents = jax.vmap(model.encoder)(data)
+            diff_data = (latents, data)
+            loss = dc.diffusion_loss(
+                model.decoder, diff_data, dc.f_neg_gamma, subkey)
+            return loss
+        
         args = self.args
         cfg = self.cfg
 
@@ -242,11 +231,18 @@ class DiffARHarness:
         log_freq = train_cfg["log_freq"]
 
         if args.ckpt:
-            self.load_checkpoint(args.ckpt)
+            #Load specified checkpoint
+            step = args.ckpt
+            ckpt_path = self.ckpt_path(step)
+            self.load_checkpoint(ckpt_path)
+        elif self.list_checkpoints():
+            #Load latest checkpoint
+            step = self.latest_ckpt_step()
+            ckpt_path = self.latest_ckpt_path()
+            #self.load_checkpoint(ckpt_path)
         else:
-            self.load_checkpoint()  # Attempt to load the latest checkpoint
-
-        step = self.most_recent_ckpt()
+            #Start from scratch and use existing init without loading a checkpoint
+            step = 0
 
         # Initialize the data downloader
         self.sharded_data_downloader.start(step * self.sharded_data_downloader.batch_size)
@@ -255,19 +251,33 @@ class DiffARHarness:
             total_loss = 0
             log_start_step = step
             while step < total_steps:
+                
+                #Save checkpoint 
+                if step % ckpt_freq == 0:
+                    print(f"Saving checkpoint for step {step}...")
+                    ckpt_path = self.ckpt_path(step)
+                    self.save_checkpoint(ckpt_path)
+                
                 step += 1
+
+                if step > 35:
+                    break
+
+                time.sleep(1)
                 
                 # Get data from the downloader
                 data = self.sharded_data_downloader.step()
+                print("a")
 
                 # Update the model
-                loss, self.state = dc.update(self.state, data, self.optimizer, self.loss_fn)
+                loss, self.state = dc.update_state_dict(self.state, data, self.optimizer, loss_fn)
+                print("b")
+                print(loss)
 
+                """
                 # Accumulate loss
                 total_loss += loss
 
-                # Acknowledge that we've processed the data
-                self.sharded_data_downloader.ack()
 
                 if step % log_freq == 0:
                     avg_loss = total_loss / (step - log_start_step)
@@ -275,16 +285,19 @@ class DiffARHarness:
                     # Reset for next logging interval
                     total_loss = 0
                     log_start_step = step
-
-                if step % ckpt_freq == 0:
-                    self.save_checkpoint(step)
+                """
+                
+                # Acknowledge that we've processed the data
+                self.sharded_data_downloader.ack()
+                print("c")
 
         except KeyboardInterrupt:
-            print("Training interrupted. Saving checkpoint...")
-            self.save_checkpoint(step)
+            print("Training interrupted.")
         finally:
             # Always stop the data downloader when we're done
+            print("d")
             self.sharded_data_downloader.stop()
+            print("e")
 
         print("Training completed.")
 
@@ -294,8 +307,3 @@ class DiffARHarness:
 
         latest_ckpt_path = self.latest_ckpt_path()
         self.load_checkpoint(latest_ckpt_path)
-
-    
-
-
-
